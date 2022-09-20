ed8b1d667dcb33733c059cb85971b64a4c162ee9
Shubhamrandale <shubhamrandale8@gmail.com>
2022-09-20 10:42:04 +0530

    Updated student and Login class

---
+++ Quizproject/src/com/java/vivek/quiz/Login.java
@@ -0,0 +1,65 @@
+package com.java.vivek.quiz;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Scanner;
+
+public class Login {
+	
+	Connection c = null;
+	PreparedStatement p1 = null;
+	
+
+	public void getLoginDetails() {
+	try {
+		Scanner sc =new Scanner(System.in);
+		ConnectionTest test = new ConnectionTest();
+		c=test.getConnection();
+		p1=c.prepareStatement("select * from student ");
+		ResultSet res = p1.executeQuery();
+		if(res.next()) {
+			
+			System.out.println("Enter your username");
+			String username = sc.nextLine();
+	
+			boolean result = username.equals(res.getString("username"));
+			if(result) {
+				System.out.println("Enter your password");
+			String password =sc.nextLine();
+			boolean result1 = password.equals(res.getString("password"));
+			if(result1) {
+				System.out.println("Press 1 to start the quiz");
+			}
+			else {
+				System.out.println("password is incorrect ");
+				System.out.println("Please enter correct password");
+				password = sc.nextLine();
+				result1 = password.equals(res.getString("password"));
+	   		}
+	            }
+			else {
+				System.out.println("Invalid username");
+				System.out.println("Please enter correct username");
+				username = sc.nextLine();
+				result = username.equals(res.getString("username"));
+				
+			}
+			
+		}
+		
+	}	
+	catch (Exception e) {
+		e.printStackTrace();
+	}	
+	}
+	public static void main(String[] args) {
+		Login l = new Login();
+		l.getLoginDetails();
+		
+		
+	}
+
+	
+
+}
--- Quizproject/src/com/java/vivek/quiz/Student.java
+++ Quizproject/src/com/java/vivek/quiz/Student.java
@@ -1,5 +1,61 @@
 package com.java.vivek.quiz;
 
-public class Student {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
+
+
+public class Student  {
+	
+	Connection con=null;
+	PreparedStatement ps =null;
+	
+	public void insertStudentData(int id, String FirstName, String LastName, String username, String password) {
+		try {
+			ConnectionTest test = new ConnectionTest();
+			con=test.getConnection();
+			ps= con.prepareStatement("insert into student (id,FirstName,LastName,username,password) values (?,?,?,?,?)");
+			ps.setInt(1, id);
+			ps.setString(2, FirstName);
+			ps.setString(3, LastName);
+			ps.setString(4, username);
+			ps.setString(5, password);
+			
+			int i = ps.executeUpdate();
+			System.out.println("Record is updated "+i);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	static Register reg= new Register();
+	public static void studentRegistry() {
+		Scanner s = new Scanner(System.in);
+		for(int i=0; i<1; i++) {
+			System.out.println("Enter your id");
+			int id = s.nextInt();
+			reg.setId(id);
+			System.out.println("Enter your FirstName");
+			String FirstName = s.next();
+			reg.setFirstName(FirstName);
+			System.out.println("Enter your LastName");
+			String LastName = s.next();
+			reg.setLastName(LastName);
+			System.out.println("Enter username");
+			String username = s.next();
+			reg.setUserName(username);
+			System.out.println("Enter your password");
+			String password = s.next();
+			reg.setPassword(password);
+			
+			Student stu =new Student();
+			stu.insertStudentData(id, FirstName, LastName, username, password);
+			
+			System.out.println(reg.toString());
+				
+		}
+		
+		
+	}
 
 }
